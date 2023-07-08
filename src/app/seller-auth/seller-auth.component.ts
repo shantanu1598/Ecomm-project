1a7c005f31e088aa78cf43f5deedbf34b0d3f8e6
Shantanu Pal <shantanupal151998@gmail.com>
2023-07-08 20:47:05 +0530

    Routing and seller signup page

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css'],
+})
+export class SellerAuthComponent {
+  constructor() {}
+  ngOnInit(): void {}
+  signup(data: object): void {
+    console.warn(data);
+  }
+}
